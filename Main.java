8e308d818e3654774255aa37078e2b70b2700d72
AKash saha <35732779+Akashsaha32@users.noreply.github.com>
2023-05-18 11:27:18 +0600

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,12 @@
+package aku.com;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Solution sol = new Solution();
+		long num = 950340;
+		String ans = sol.numberToWords(num);
+		System.out.println(ans);
+	}
+
+}
